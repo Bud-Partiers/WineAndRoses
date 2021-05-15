1186f37010397ce11855d7d5fddf9de7e44056bb
Nick Stricker <strickerdev@gmail.com>
2021-05-14 19:04:32 -0500

    section functionality, merch drop

--- roses/src/App.js
+++ roses/src/App.js
@@ -3,7 +3,21 @@ import './App.scss';
 import greenBoquet from './images/green-bouquet.jpg';
 import roseVase from './images/rose-vase.jpg';
 
+import bpBlack from './images/merch/budparty-black.jpg'
+import bpPink from './images/merch/budparty-pink.jpg'
+import bpPose from './images/merch/budparty-pose.jpg'
+import bpRed from './images/merch/budparty-red.jpg'
+import bpShirt from './images/merch/budparty-shirt.jpg'
+
+
 function App() {
+
+  let displaySections = [
+    { title:'Shirt Designs',
+      images:[bpPose, bpShirt,bpBlack, bpPink, bpRed]
+    }
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,7 +31,6 @@ function App() {
         </div>
         <div className="info">
           <h2 className='contact-title'>BOOK A PRIVATE CLASS</h2>
-          <a className='msg' href='https://m.me/flowersandclasses?fbclid=IwAR3IgNUGCt8RWVpBAjfbKPDpaM_xgi4r1uMzCJa0_EtgQ5gnPNLlJtieh-E'>Send a Message</a>
           <div className='contacts'>
 
             <div className='item'>
@@ -30,8 +43,24 @@ function App() {
             </div>
 
           </div>
+          <a className='msg' href='https://m.me/flowersandclasses?fbclid=IwAR3IgNUGCt8RWVpBAjfbKPDpaM_xgi4r1uMzCJa0_EtgQ5gnPNLlJtieh-E'>Send a Message</a>
+          
         </div>
 
+        {displaySections.map((item) => {
+          return (<div className='section'>
+                    <h2>{item.title}</h2>
+                    <div className='sub-section'>
+                      {item.images.map((img, index) => {
+                        return <img className='sub-section-img' src={img}/>
+                    
+                      })
+                    }
+                    </div>
+                  </div>)
+            
+          })
+        }
 
       </div>
     </div>
